7dbde7e257a28b4d451ca71da76b08ee79080743
tatsuya.satake <tatsuya.satake.5x@stu.hosei.ac.jp>
2019-02-09 06:54:14 +0000

    migrate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
     def status
     end
+    validates :content, presence: true, length: { maximum: 10 }
     validates :status, presence: true, length: { maximum: 10 }
 end
 
