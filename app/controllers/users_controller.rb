87f926f3840f4c168e4c7f65bae112a661d05789
tatsuya.satake <tatsuya.satake.5x@stu.hosei.ac.jp>
2019-02-16 07:21:58 +0000

    fix migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,4 +31,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
